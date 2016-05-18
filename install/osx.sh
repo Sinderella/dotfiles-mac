ea1c47256885298aa3ce974d6d395bc9f47edac1
Sinderella <rawsec@gmail.com>
2016-05-18 16:37:25 +0100

    Disabled sound on boot

--- install/osx.sh
+++ install/osx.sh
@@ -4,4 +4,5 @@
 git config --global credential.helper osxkeychain; # activate git credentials storage
 git config --global push.default simple; # default Git push behavior is set to `simple`
 
-
+# Disable sound on boot
+sudo nvram SystemAudioVolume=" "
